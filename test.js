3588f5852215050022da2caddfc8830d9950a35c
chickn-noodle <mud.puddle869@gmail.com>
2024-04-05 22:06:53 +0500

    removed test file

--- test.js
+++
@@ -1,7 +0,0 @@
-(async () => {
-    const patch = await fetch("https://deschtimes.com/api/v1/groups/bGVnSZkkyrXyNtUf3b8unw/shows/moto/staff?finished=true&member=444182805493186574&position=tl", {
-    method: "PATCH"
-})
-    const data = await patch.json()
-console.log(data)
-})()
