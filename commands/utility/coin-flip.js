ab3e4efdf2b68afa4a26dcbdac880fc5f40292b6
chickn-noodle <mud.puddle869@gmail.com>
2024-04-07 21:23:52 +0500

    added Coinflip command

---
+++ commands/utility/coin-flip.js
@@ -0,0 +1,24 @@
+const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('coin-flip')
+		.setDescription('flip a coin to make a decision')
+		.addStringOption(option =>
+			option.setName('options')
+				.setDescription('which options to choose from (comma separated')
+                .setRequired(true))
+        .setDefaultMemberPermissions(PermissionFlagsBits.MentionEveryone),
+	async execute(interaction) {
+        if(Math.floor(Math.random() * 1000) == 0) {
+            interaction.reply("The coin landed on its side!")
+            return
+        }
+
+        const optionArray = interaction.options.getString('options').split(",");
+        optionArray.forEach(element => {element = element.trim()});
+        console.log(optionArray);
+        
+        interaction.reply(`The coin chose ${optionArray[Math.floor(Math.random() * optionArray.length)]}`)
+    }
+};
